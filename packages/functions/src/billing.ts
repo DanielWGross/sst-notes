6cc873c011b7c2356995d6eb0cb226d4acefdf5e
Daniel Gross <danwgross@gmail.com>
2024-11-25 21:51:28 -0500

    Adding unit tests

--- packages/functions/src/billing.ts
+++ packages/functions/src/billing.ts
@@ -2,9 +2,17 @@ import Stripe from "stripe";
 import { Resource } from "sst";
 import { Util } from "@sst-notes/core/util";
 import { Billing } from "@sst-notes/core/billing";
+import { z } from "zod";
+
+const BillingSchema = z.object({
+  storage: z.number(),
+  source: z.string(),
+});
 
 export const main = Util.handler(async (event) => {
-  const { storage, source } = JSON.parse(event.body || "{}");
+  const { storage, source } = BillingSchema.parse(
+    JSON.parse(event.body || "{}")
+  );
   const amount = Billing.compute(storage);
   const description = "Scratch charge";
 
