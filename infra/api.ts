05f3dc08181b84f66af8a651ede690fb40c29d20
Daniel Gross <danwgross@gmail.com>
2024-12-04 22:23:23 -0500

    Setting up custom domains

--- infra/api.ts
+++ infra/api.ts
@@ -1,6 +1,7 @@
 import { table, secret } from "./storage";
 
 export const api = new sst.aws.ApiGatewayV2("Api", {
+  domain: $app.stage === "production" ? "api.notes.dangross.me" : undefined,
   transform: {
     route: {
       handler: {
